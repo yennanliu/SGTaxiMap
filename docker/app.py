b60d078eef557698614570796b9877e84e240531
yennanliu <f339339@gmail.com>
2019-01-08 16:49:34 +0000

    make docker flask app port back to 5000

--- docker/app.py
+++ docker/app.py
@@ -28,4 +28,4 @@ def hello():
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
