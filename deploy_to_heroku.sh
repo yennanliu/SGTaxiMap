3b821ef7c65df9c07ebb135057d8b3ba30bfff94
yennanliue <f339339@gmail.com>
2019-06-29 16:54:15 +0800

    add comment how to check the deploy log

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -7,4 +7,5 @@
 heroku create sgtaxi-heroku-app --buildpack heroku/python
 heroku git:remote -a sgtaxi-heroku-app
 git push heroku master
+echo 'deploy... please check the deploy log via command : heroku logs --tail'
 echo 'deploy OK, please visit the app via http://sgtaxi-heroku-app.herokuapp.com'
