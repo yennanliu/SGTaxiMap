f1f67469883eb6c5baeb2759282e22e33ca2e499
yennanliu <f339339@gmail.com>
2019-07-10 14:42:35 +0800

    rename test for app pages

--- tests/app_page_test.py
+++ tests/app_page_test.py
@@ -3,15 +3,15 @@ import requests
 import json
 import pytest
 
-def test_dummy():
+def test_helloworld():
     response = requests.get('http://0.0.0.0:5000/hello_world')
     assert response.status_code == 200
 
-def test_get_taxi_location():
+def test_main_page():
     response = requests.get('http://0.0.0.0:5000/')
     print (response)
     assert response.status_code == 200
 
 if __name__ == "__main__":
-    test_dummy()
-    test_get_taxi_location()
+    test_helloworld()
+    test_main_page()
