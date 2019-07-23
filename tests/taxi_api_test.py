71399e3bfd2e3dce51e2766714f1845d4b308f6c
yennanliu <f339339@gmail.com>
2019-07-23 16:32:06 +0800

    add more fetch data test (data type, data count..)

--- tests/taxi_api_test.py
+++ tests/taxi_api_test.py
@@ -2,8 +2,16 @@ import pytest, unittest
 from controller import call_taxi_api
 
 def test_call_taxi_api():
-	result, response = call_taxi_api()
-	assert response.code == 200 
+    result, response = call_taxi_api()
+    assert response.code == 200 
+
+def test_taxi_data_type():
+    result, response = call_taxi_api()
+    assert type(result) == dict  
+
+def test_taxi_data_not_null():
+    result, response = call_taxi_api()
+    assert len(result['features'][0]['geometry']['coordinates']) > 0 
 
 if __name__ == '__main__':
     pytest.main([__file__])
