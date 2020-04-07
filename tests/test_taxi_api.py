01bcdeb174cd9fa785348cc8b32b00aa18376084
yennanliu <f339339@gmail.com>
2020-04-07 18:28:04 +0800

    use pytest.fixture avoid repeat code (test_taxi_api.py)

--- tests/test_taxi_api.py
+++ tests/test_taxi_api.py
@@ -3,18 +3,23 @@ import sys
 sys.path.append(".") 
 from controller import Taxi
 
-taxi = Taxi()
+#taxi = Taxi()
 
-def test_call_taxi_api():
-    result, response = taxi.call_taxi_api()
+@pytest.fixture(scope="module")
+def initTaxi():
+	taxi = Taxi()
+	return taxi
+
+def test_call_taxi_api(initTaxi):
+    result, response = initTaxi.call_taxi_api()
     assert response.code == 200 
 
-def test_taxi_data_type():
-    result, response = taxi.call_taxi_api()
+def test_taxi_data_type(initTaxi):
+    result, response = initTaxi.call_taxi_api()
     assert type(result) == dict  
 
-def test_taxi_data_not_null():
-    result, response = taxi.call_taxi_api()
+def test_taxi_data_not_null(initTaxi):
+    result, response = initTaxi.call_taxi_api()
     assert len(result['features'][0]['geometry']['coordinates']) > 0 
 
 if __name__ == '__main__':
